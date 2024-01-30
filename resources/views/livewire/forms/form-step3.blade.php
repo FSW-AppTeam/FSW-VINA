eb67ad36b29b0585cab39281720e10be45239f0e
Daan Asscheman <d.asscheman@uu.nl>
2024-01-30 16:18:29 +0100

    refactor: Update form-step4.blade.php, form-step2.blade.php, and form-step3.blade.php (feature/da_next_button)
    **Repository:** /home/daan/Development/FSW-Dualnets
    
    - Update form-step4.blade.php: Fix formatting and add nextEnabled and backEnabled props to x-layouts.form component.
    - Update form-step2.blade.php: Fix formatting and add nextEnabled and backEnabled props to x-layouts.form component.
    - Update form-step3.blade.php: Fix formatting and add nextEnabled and backEnabled props to x-layouts.form component.
    
    This commit message is auto generated with AI.

--- resources/views/livewire/forms/form-step3.blade.php
+++ resources/views/livewire/forms/form-step3.blade.php
@@ -1,10 +1,11 @@
-<x-layouts.form :step-id="$stepId" :json-question="$jsonQuestion">
+<x-layouts.form :step-id="$stepId"
+                :next-enabled="$nextEnabled"
+                :back-enabled="$backEnabled"
+                :json-question="$jsonQuestion">
     <div id="scope-form-step3" class="mt-4">
-            @if($setPage)
-                <div class="form-group set-fade-in">
-                    <h6 class="pb-2">{{ $jsonQuestion->question_content }}</h6>
-                    <input type="number" wire:model="age" name="age" class="form-control style-input" value="{{ $age }}"/>
-                </div>
-            @endif
+            <div class="form-group set-fade-in">
+                <h6 class="pb-2">{{ $jsonQuestion->question_content }}</h6>
+                <input type="number" wire:model.live="age" name="age" class="form-control style-input" value="{{ $age }}"/>
+            </div>
         </div>
 </x-layouts.form>
