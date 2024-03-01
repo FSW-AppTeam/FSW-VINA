46573e1364aa84733ce00e59772608d69b756885
Daan Asscheman <d.asscheman@uu.nl>
2024-03-01 10:41:23 +0100

    Voeg functie toe om vlag op te halen voor specifieke locatie (develop) **Dit bericht is automatisch gegenereerd met AI.**

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -255,3 +255,44 @@ if (! function_exists('getIsoCountries')) {
 
     }
 }
+if (! function_exists('csvToArray')) {
+    function csvToArray($filename = '', $headers = null, $delimiter = ';')
+    {
+        if (! file_exists($filename) || ! is_readable($filename)) {
+            return false;
+        }
+
+        $data = [];
+        if (($handle = fopen($filename, 'r')) !== false) {
+            $headerSet = false;
+            while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
+                if (empty($headers) && ! $headerSet) {
+                    $headers = $row;
+                    $headerSet = true;
+
+                    continue;
+                }
+                $data[] = array_combine($headers, $row);
+            }
+            fclose($handle);
+        }
+
+        return $data;
+    }
+}
+
+if (! function_exists('get_flag_for_locale')) {
+    function get_flag_for_locale($locale): string
+    {
+        switch ($locale) {
+            case 'en':
+                return 'gb';
+            case 'nl':
+                return 'nl';
+            case 'fr':
+                return 'fr';
+            default:
+                return 'sr';
+        }
+    }
+}
