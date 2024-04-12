6b5249957827e14372977552414f68f7df25d030
Daan Asscheman <d.asscheman@uu.nl>
2024-04-12 13:40:58 +0200

    Skip question 22 during pilot.

--- resources/views/livewire/forms/form-step22.blade.php
+++ resources/views/livewire/forms/form-step22.blade.php
@@ -17,56 +17,56 @@
             <div class="text-center mb-2 wrapper-shadow-list">
                 <div class="block-student-active shadow-row-step22"  >
 
-                    <div class="block-students-vertical relation-block-student-shadow" data-student-list >
-                        @foreach($shadowStudents as $key => $student)
-                            <div class="justify-content-center text-center student-shadow-flex @if($key !== 0) fadeOut @endif" >
+{{--                    <div class="block-students-vertical relation-block-student-shadow" data-student-list >--}}
+{{--                        @foreach($shadowStudents as $key => $student)--}}
+{{--                            <div class="justify-content-center text-center student-shadow-flex @if($key !== 0) fadeOut @endif" >--}}
 
-                                <div class="p-2 btn-circle btn-xl studentBtn title">
-                                        <?= $student['id'] === $selfStudentId ? $selfText : $this->getStudentById($student['id'])['name']; ?>
-                                </div>
+{{--                                <div class="p-2 btn-circle btn-xl studentBtn title">--}}
+{{--                                        <?= $student['id'] === $selfStudentId ? $selfText : $this->getStudentById($student['id'])['name']; ?>--}}
+{{--                                </div>--}}
 
-                                <h2 class="circle-amp-student right">&#38;</h2>
+{{--                                <h2 class="circle-amp-student right">&#38;</h2>--}}
 
-                                <div class="p-2 btn-circle btn-xl studentBtn title">
-                                        <?= $student['relation_id'] === $selfStudentId ? $selfText : $this->getStudentById($student['relation_id'])['name']; ?>
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
+{{--                                <div class="p-2 btn-circle btn-xl studentBtn title">--}}
+{{--                                        <?= $student['relation_id'] === $selfStudentId ? $selfText : $this->getStudentById($student['relation_id'])['name']; ?>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        @endforeach--}}
+{{--                    </div>--}}
                 </div>
             </div>
 
-            <div class="container-sm">
-                <div class="row justify-content-center align-items-center text-center">
-                    <div class="col-12 col-lg-12">
-                        @if(!empty($answerSelected))
-                            <button type="button" data-start-square
-                                    wire:click="$dispatchset-square-animation', {event:event});"
-                                    id="{{$answerSelected['id']}}"
-                                    class="btn btn-outline-secondary press-buttons-inline rounded"
-                                    style="height: 50px; border: solid 2px orange;padding-top: 10px;">
-                                {{$answerSelected['value']}}
-                            </button>
-                        @else
-                            <div id="set-square-area" class="btn btn-outline-secondary press-buttons-inline rounded"></div>
-                        @endif
-                    </div>
-                </div>
-            </div>
+{{--            <div class="container-sm">--}}
+{{--                <div class="row justify-content-center align-items-center text-center">--}}
+{{--                    <div class="col-12 col-lg-12">--}}
+{{--                        @if(!empty($answerSelected))--}}
+{{--                            <button type="button" data-start-square--}}
+{{--                                    wire:click="$dispatchset-square-animation', {event:event});"--}}
+{{--                                    id="{{$answerSelected['id']}}"--}}
+{{--                                    class="btn btn-outline-secondary press-buttons-inline rounded"--}}
+{{--                                    style="height: 50px; border: solid 2px orange;padding-top: 10px;">--}}
+{{--                                {{$answerSelected['value']}}--}}
+{{--                            </button>--}}
+{{--                        @else--}}
+{{--                            <div id="set-square-area" class="btn btn-outline-secondary press-buttons-inline rounded"></div>--}}
+{{--                        @endif--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
 
-            <div class="form-group mt-2">
-                <div class="container-sm">
-                    <div class="row justify-content-center align-items-center">
-                        <div class="col-12 col-lg-10">
-                            @foreach ($jsonQuestion->question_answer_options as $answer)
-                                <livewire:partials.answer-btn-block :id="$answer->id" :value="ucfirst($answer->value)"
-                                                                    :answer-selected="$answerSelected"
-                                                                    wire:key="{{ $answer->id . time() }}"/>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="form-group mt-2">--}}
+{{--                <div class="container-sm">--}}
+{{--                    <div class="row justify-content-center align-items-center">--}}
+{{--                        <div class="col-12 col-lg-10">--}}
+{{--                            @foreach ($jsonQuestion->question_answer_options as $answer)--}}
+{{--                                <livewire:partials.answer-btn-block :id="$answer->id" :value="ucfirst($answer->value)"--}}
+{{--                                                                    :answer-selected="$answerSelected"--}}
+{{--                                                                    wire:key="{{ $answer->id . time() }}"/>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         @endif
     </div>
 </x-layouts.form>
